f335d0e832ca4fac00f6238d5fa6adaafbc54680
ahmedhamdyhasan <dev.ahmed.hamdy.h010@gmail.com>
2024-05-04 02:18:57 +0300

    create routes and controllers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from './routes/userRoute.js'
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_CONNECTION)
   .then(() => console.log("Connected to DB"))
   .catch((e) => console.log(e));
 
+
+ 
 const app = express();
+app.use ('api/user',userRouter)
 app.listen(3000, () => {
   console.log("Server is running on port 3000 ");
 });
