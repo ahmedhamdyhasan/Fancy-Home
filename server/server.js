162a1322a7b5ae47bcf9e60d766a407b5f4e4241
ahmedhamdyhasan <dev.ahmed.hamdy.h010@gmail.com>
2024-05-04 01:57:02 +0300

    setting the server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
-import mongoose from 'mongoose'
-import express from 'express'
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => console.log("Connected to DB"))
+  .catch((e) => console.log(e));
 
-
-const app = express()
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000 ')
-})
+const app = express();
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 ");
+});
